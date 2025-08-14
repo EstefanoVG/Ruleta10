012c885d1f5b2ce1187071abb9816a038d5d993e
EstefanoVG <54048861+EstefanoVG@users.noreply.github.com>
2025-08-13 23:42:49 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = 'ruleta-v1';
+const assets = [
+  './ruleta.html',
+  './manifest.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== cacheName).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
