b051dc84174fae385d4f1597cf32859927ff39cb
EstefanoVG <54048861+EstefanoVG@users.noreply.github.com>
2025-08-14 00:25:27 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'ruleta-v1';
+const cacheName = 'ruleta-v3'; // súbelo (v2, v3, etc.)
 const assets = [
   './ruleta.html',
   './manifest.json',
@@ -8,16 +8,16 @@ const assets = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(assets))
-  );
+  self.skipWaiting(); // activa SW nuevo inmediatamente
+  event.waitUntil(caches.open(cacheName).then((c) => c.addAll(assets)));
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== cacheName).map(k => caches.delete(k))
-    ))
+    Promise.all([
+      caches.keys().then(keys => Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))),
+      self.clients.claim() // toma control de las pestañas abiertas
+    ])
   );
 });
 
